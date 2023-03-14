8d58b86158f5c59f0607e630ec6fb196b0e0531b
Kafui Kpoh <qaphuikpoh@gmail.com>
2023-03-14 16:09:33 +0000

    - add new json data - modify json parsing - modify detail view to display ingredient name

--- RecipeApp/Services/DataService.swift
+++ RecipeApp/Services/DataService.swift
@@ -38,6 +38,10 @@ class DataService {
                 //add the unique IDs
                 for r in recipeData {
                     r.id = UUID()
+                    
+                    for i in r.ingredients {
+                        i.id = UUID()
+                    }
                 }
                 
                 //return the recipes
